3ea7d52439e0fd0e4a916d854dfb4383df6bba80
Dane P Dawson <dane.p.dawson@gmail.com>
2020-01-20 01:25:10 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,9 +2,9 @@ def nyc_pigeon_organizer(data)
   pigeon_list = {}
   #pigeon_list outputs = {"names"{:color["description"], :gender["description"], :lives["locations"]}}
   #data inputs = {:color{:purple["names"], :grey["names"], :white["names"], :brown["names"]}, :gender{:male["names"], :female["names"]}, :lives{"locations"["names"]}}
-  each.data ({}) do |info| #search data hash and for each element that describes information
-    each.info({}) do |description| #search each information hash and for each element description of possible element
-      each.description({}) do |name| #search each description of identifier for each name.
+  data.keys().each do |info| #search data hash and for each element that describes information
+    info.keys().each do |description| #search each information hash and for each element description of possible element
+      description.keys().each do |name| #search each description of identifier for each name.
         if pigeon_list.key?("#{name}") #if pigeon_list hash contains an element matching that name
           #check if current data point exists inside the corresponding array
         else #if not, create a new element in pigeon_list that contains the information according to this names current name, info, and description
