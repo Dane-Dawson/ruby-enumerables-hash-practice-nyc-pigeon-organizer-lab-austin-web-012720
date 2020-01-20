940e5a8d1aeb94bdacb51db36afe550e9b4703fb
Dane P Dawson <dane.p.dawson@gmail.com>
2020-01-20 01:22:35 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -8,7 +8,7 @@ def nyc_pigeon_organizer(data)
         if pigeon_list.key?("#{name}") #if pigeon_list hash contains an element matching that name
           #check if current data point exists inside the corresponding array
         else #if not, create a new element in pigeon_list that contains the information according to this names current name, info, and description
-         pidgeon_list["#{name}"][:info] = "#{description}"
+         pigeon_list["#{name}"][:info] = "#{description}"
        end
       end
     end
